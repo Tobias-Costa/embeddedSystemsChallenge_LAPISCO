3c43ee1bb5b40a8db629ab77cccc4909244aa65d
Tobias <tobiascosta064@gmail.com>
2026-05-20 15:55:27 -0300

    feat: django http streaming done

--- person_detection_app/urls.py
+++ person_detection_app/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('person/detection/', views.detection, name='detection'),
-    path('person/heatmap/', views.heatmap, name='heatmap'),
+    path("", views.index, name="index"),
+    path("person/detection/", views.detection, name="detection"),
+    path("person/heatmap/", views.heatmap, name="heatmap"),
+    path("person/dashboard/", views.dashboard, name="dashboard"),
+    path("person/api/<str:mode>", views.camera_api, name="camera_api"),
 ]
--- person_detection_app/views.py
+++ person_detection_app/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, StreamingHttpResponse
+from . import camera
 
 # Create your views here.
 
+
 def index(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
 
 def detection(request):
     return render(request, "person_detection.html")
 
+
 def heatmap(request):
     return render(request, "person_heatmap.html")
+
+
+def dashboard(request):
+    return render(request, "dashboard.html")
+
+
+def camera_api(request, mode):
+    return StreamingHttpResponse(
+        camera.generate_stream(mode),
+        content_type="multipart/x-mixed-replace;boundary=frame",
+    )
+
+
+# def heatmap_api(request, mode):
+#     return StreamingHttpResponse(
+#         camera.generate_stream(mode),
+#         content_type="multipart/x-mixed-replace;boundary=frame",
+#     )
