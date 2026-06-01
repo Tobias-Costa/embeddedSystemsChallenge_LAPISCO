6f2d601f0355209cecefd46bbb3aaa3c05bb78e7
Tobias <tobiascosta064@gmail.com>
2026-05-31 23:53:21 -0300

    feat: deleted unecessary routes

--- person_detection_app/urls.py
+++ person_detection_app/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("person/detection/", views.detection, name="detection"),
-    path("person/heatmap/", views.heatmap, name="heatmap"),
-    path("person/dashboard/", views.dashboard, name="dashboard"),
-    path("person/api/<str:mode>", views.camera_api, name="camera_api"),
+    path("detection/", views.dashboard, name="dashboard"),
+    path("api/", views.camera_api, name="camera_api"),
 ]
--- person_detection_app/views.py
+++ person_detection_app/views.py
@@ -4,32 +4,12 @@ from . import camera
 
 # Create your views here.
 
-
-def index(request):
-    return render(request, "index.html")
-
-
-def detection(request):
-    return render(request, "person_detection.html")
-
-
-def heatmap(request):
-    return render(request, "person_heatmap.html")
-
-
 def dashboard(request):
     return render(request, "dashboard.html")
 
 
-def camera_api(request, mode):
+def camera_api(request):
     return StreamingHttpResponse(
-        camera.generate_stream(mode),
+        camera.generate_stream(),
         content_type="multipart/x-mixed-replace;boundary=frame",
     )
-
-
-# def heatmap_api(request, mode):
-#     return StreamingHttpResponse(
-#         camera.generate_stream(mode),
-#         content_type="multipart/x-mixed-replace;boundary=frame",
-#     )
