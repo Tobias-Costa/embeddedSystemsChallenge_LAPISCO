c770cdece65288a23f99d669534de9ac3272376d
Tobias <tobiascosta064@gmail.com>
2026-06-03 13:15:07 -0300

    refactor: comented views and unecessary imports deleted

--- person_detection_app/urls.py
+++ person_detection_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+# Rotas do app 'person_detection_app'
 urlpatterns = [
     path("detection/", views.dashboard, name="dashboard"),
     path("api/", views.camera_api, name="camera_api"),
--- person_detection_app/views.py
+++ person_detection_app/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse, StreamingHttpResponse
-from . import camera
+from django.http import StreamingHttpResponse
+from camera import generate_stream
 
 # Create your views here.
 
+# View de dashboard com header e imagens da stream
 def dashboard(request):
     return render(request, "dashboard.html")
 
-
+# Rota API que retorna um MJPEG contínuo
 def camera_api(request):
     return StreamingHttpResponse(
-        camera.generate_stream(),
+        generate_stream(),
         content_type="multipart/x-mixed-replace;boundary=frame",
     )
